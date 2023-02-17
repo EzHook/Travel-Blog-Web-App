7f5d49af5edf81d9b732fc4912fe981845f4dc0c
Narendra Naik <113234585+EzHook@users.noreply.github.com>
2023-02-17 11:50:40 +0530

    Add files via upload
    changed config for backend

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require ("express");
+const app = express();
+const mongoose = require("mongoose");
+const { default: postRouter } = require("./routing/post-routes");
+const { default: useRouter } = require("./routing/user-routes");
+require("dotenv").config();
+const cors = require ("cors");
+const mongoURL = process.env.MONGODB_CLIENT_ID;
+const PORT = process.env.PORT || 5000;
+
+//middleware
+
+app.use(cors());
+app.use(express.json());
+app.use("/user", useRouter);
+app.use("/posts", postRouter);
+
+//connections
+mongoose.connect(mongoURL)
+.then(()=> {
+    app.listen(PORT, (req, res)=> {
+        console.log("We are live on port");
+    });
+})
+.catch((error)=> console.log(error));
+
